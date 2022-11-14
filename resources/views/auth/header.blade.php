bbfb91ae84269c6623d7523d903878680404ab85
Nguyen Thuan <nguyenthuan1694@gmail.com>
2022-11-14 21:59:20 +0700

    'updatetitle'

--- resources/views/auth/header.blade.php
+++ resources/views/auth/header.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>PSOFT</title>
+    <title>BOOKSTORE</title>
 
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
